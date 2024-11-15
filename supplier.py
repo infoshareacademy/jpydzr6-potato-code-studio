87233d20c042c36277e58458d66a589b021eb46e
Wojciech Wawrzyszko <wuwu1978@o2.pl>
2024-11-15 18:37:18 +0100

    Changes in supplier class

--- supplier.py
+++ supplier.py
@@ -1,25 +1,34 @@
 import json
-from user import User
 
-class Supplier(User):
+class Supplier:
 
-    __slots__ = ("f_name", "l_name", "address", "email", "telephone", "is_supplier", "suppliers", "category")
+    __slots__ = ("supplier_f_name", "supplier_l_name", "supplier_address", "supplier_email", "supplier_telephone", "suppliers", "category")
 
-    def __init__(self, f_name: str, l_name: str, address: str, email: str, telephone: str, is_supplier: bool):
-        # Inheritance
-        super().__init__(f_name, l_name, address, email, telephone, loyalty_points=0)
-        # Attributes of this class
-        self.is_supplier = True
+    def __init__(self, supplier_f_name: str, supplier_l_name: str, supplier_address: str, supplier_email: str,
+                 supplier_telephone: str):
+        self.supplier_f_name = supplier_f_name
+        self.supplier_l_name = supplier_l_name
+        self.supplier_address = supplier_address
+        self.supplier_email = supplier_email
+        self.supplier_telephone = supplier_telephone
         self.suppliers = {}
         self.category = {}
 
-    def add_supplier_name(self, f_name: str, l_name: str) -> None:
-        full_name = f"{f_name} {l_name}"
-        self.suppliers[full_name] = {"first_name": f_name, "last_name": l_name}
+    def __str__(self) -> str:
+        return (
+            f"\nImię i nazwisko dostawcy: {self.supplier_f_name} {self.supplier_l_name}"
+            f"\nAdres dostawcy: {self.supplier_address}"
+            f"\nE-mail dostawcy: {self.supplier_email}"
+            f"\nTelefon dostawcy: {self.supplier_telephone}"
+        )
 
-    def add_supplier_details(self, address: str, email: str, telephone: str) -> None:
-        full_address = f"{address}, {email}, {telephone}"
-        self.suppliers[full_address] = {"customer_address": address, "email": email, "telephone": telephone}
+    def add_supplier_name(self, supplier_f_name: str, supplier_l_name: str) -> None:
+        full_name = f"{supplier_f_name} {supplier_l_name}"
+        self.suppliers[full_name] = {"supplier_first_name": supplier_f_name, "supplier_last_name": supplier_l_name}
+
+    def add_supplier_details(self, supplier_address: str, supplier_email: str, supplier_telephone: str) -> None:
+        full_address = f"{supplier_address}, {supplier_email}, {supplier_telephone}"
+        self.suppliers[full_address] = {"supplier_address": supplier_address, "supplier_email": supplier_email, "telephone": supplier_telephone}
 
     def get_suppliers(self) -> dict:
         return self.suppliers
@@ -37,13 +46,14 @@ class Supplier(User):
 
 ### TESTING ###
 s1 = Supplier("Jan", "Jabłko", "Jabłkowa 1, 10-100 Jabłoniowo", "jablko@wp.pl",
-              "100 200 100", True)
+              "100 200 100")
 s2 = Supplier("Grzegorz", "Gruszka", "Gruszkowa 5, 50-550 Gruszkowo", "gruszek@gmail.com",
-              "605 605 605", True)
+              "605 605 605")
 
-# if __name__ == '__main__':
-#     print(s1.get_suppliers())
-#     s1.add_supplier_name("Basia", "Basiowska")
-#     print(s1.get_suppliers())
-#     s1.save_suppliers_to_file()
-#     s1.save_categories_to_file()
+if __name__ == '__main__':
+    print(s1.get_suppliers())
+    s1.add_supplier_name("Basia", "Basiowska")
+    print(s1.get_suppliers())
+    s1.save_suppliers_to_file()
+    s1.save_categories_to_file()
+    print(str(s1))
