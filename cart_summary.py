c8f26099c0fec0608608ddd643ad16e18868dbaa
Julia <juliablaumann@gmail.com>
2024-11-19 11:17:11 +0100

    change of nomenclature to English

--- cart_summary.py
+++ cart_summary.py
@@ -38,7 +38,7 @@ class CartSummary(Mod_loyalty):
         else:
             raise TypeError("Produkt musi być instancją klasy Product")
 
-def wyswietl_produkty(products):
+def display_products(products):
     if not products:
         print("Brak produktów do wyświetlenia.")
         return
@@ -53,7 +53,7 @@ def normalize_string(input_string: str) -> str:
     return normalized.lower()
 
 def add_product_to_cart(cart, available_products):
-    wyswietl_produkty(available_products)
+    display_products(available_products)
     product_choice = input("Podaj nazwę produktu, który chcesz dodać: ")
     normalized_choice = normalize_string(product_choice)
     product_added = False  # Flag to check if product was found
@@ -61,7 +61,6 @@ def add_product_to_cart(cart, available_products):
         if normalize_string(product.name_tag) == normalized_choice:
             cart.add_product(product)
             print(f"Produkt {product.name_tag} dodano do koszyka.")
-            product_added = True
             return product
     if not product_added:
         print("Nie znaleziono produktu o podanej nazwie.")
