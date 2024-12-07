fc972821437837eb3f08f479df0be1fedc740719
PawelEnerlich <pawel.enerlich.kurs@gmail.com>
2024-12-07 18:45:27 +0100

    Updating payment method

--- payment.py
+++ payment.py
@@ -1,55 +1,60 @@
 from cart import Cart
 
-cart = Cart()
-
-class Payment(Cart):
-    def __init__(self):
-        super().__init__()
-
-    def make_basket_empty(self) -> None:
-            self.basket = []
-
-    def protection_payment(self) -> None:
-        if not self.basket:
-            print("Twój koszyk jest pusty nie możesz przejść do płatności.")
-            return
-        else:
-            payment_menu()
-
-def payment_menu(cart_instance:Cart):
+def first_option_in_payment_choice(cart_pay:Cart):
     while True:
-        payment_choice = input("[1] Przejście do zapłaty\n"
+        final_choice = input("Czy potwierdzasz płatność tak / nie ?\n"
+                             "Twój wybór:")
+        match final_choice:
+            case "tak":
+                while True:
+                    print("Brawo właśnie kupiłeś u nas produkt.\n")
+                    cart_pay.basket.clear()
+                    return
+
+            case "nie":
+                return
+            case _:
+                print("Spróbuj jeszcze raz. Podałeś zły wybór.\n")
+
+def confirmation_email() -> str:
+        email = input("Podaj e-mail, a wyślemy Tobie potwierdzenie zakupu.\n"
+                      "Twój e-mail:")
+        return email
+
+def payment_menu(cart_pay:Cart):
+    while True:
+        payment_choice = input("[1] Kup\n"
                                "[2] Powrót do menu\n"
                                "Twój wybór: ")
-
         match payment_choice:
-
             case "1":
-                while True:
-                    final_choice = input("Czy na pewno chcesz zapłacić tak / nie ?")
+                confirmation_email()
+                first_option_in_payment_choice(cart_pay)
+                return
 
-                    match final_choice:
-                        case "tak":
-                            while True:
-                                print("Brawo właśnie kupiłeś u nas produkt.")
-                                cart_instance.basket.clear()
-                                print("Twój koszyk został opróżniony.")
-                                return
-
-                        case "nie":
-                            return
-                        case _:
-                            print("Spróbuj jeszcze raz. Podałeś zły wybór.")
             case "2":
                 break
 
             case _:
-                print("Podałeś zły wybór. Spróbuj jeszcze raz.")
+                print("Podałeś zły wybór. Spróbuj jeszcze raz.\n")
 
+class Payment:
+    def __init__(self, cart_pay):
+        self.cart = cart_pay
 
-
-
-
-
-
-
+    def final_payment(self) -> None:
+        if not self.cart.basket:
+            return
+        else:
+            while True:
+                move_to_payment = input("Czy chcesz przejść do płatności? tak / nie: ")
+                match move_to_payment:
+                    case "tak":
+                        self.cart.total_price()
+                        payment_menu(self.cart)
+                        break
+                    case "nie":
+                        print("Wracasz do menu głównego.\n")
+                        break
+                    case _:
+                        print("Podałeś zły wybór. Spróbuj jeszcze raz.\n")
