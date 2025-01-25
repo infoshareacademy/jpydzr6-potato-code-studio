2e86bddd9064a32ffe9e3d70afaf8956bd5659a1
mKusztal <maciej.kusztal1995@gmail.com>
2025-01-25 15:53:45 +0200

    fix: update django admin panel

--- bio_market_store/store/admin.py
+++ bio_market_store/store/admin.py
@@ -1,11 +1,66 @@
 from django.contrib import admin
-
-from .models import UserProfile
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import UserProfile, Address
 
 
 # Register your models here.
+class UserCreatingForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = UserProfile
+        fields = ("username", "email", "password")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password"].widget = forms.PasswordInput()
+
+
+class UserPanelChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = UserProfile
+        fields = ("username", "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password"].widget = forms.PasswordInput()
+
+
+class AddressInline(admin.StackedInline):
+    model = Address
+    can_delete = False
+
+
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "first_name", "last_name")
+    add_form = UserCreatingForm
+    change_form = UserPanelChangeForm
+    inlines = (AddressInline,)
+    fieldsets = [
+        (None, {"fields": ("username", "password", "email")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email"),
+            },
+        ),
+    ]
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
--- bio_market_store/store/models.py
+++ bio_market_store/store/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from store.utils.user_validator import UserValidator
 
 
 class UserProfile(AbstractUser):
+    user_validation = UserValidator()
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    password = models.CharField(
+        max_length=128,
+        help_text=user_validation.get_help_text(),
+        validators=[user_validation.validate],
+    )
+
     def __str__(self):
         return f"{self.username}"
 
