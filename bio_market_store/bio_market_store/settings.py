30dd7843283eb261ff4d2021280e2514ed361c14
Wojciech Wawrzyszko <wuwu1978@o2.pl>
2025-01-28 23:48:32 +0100

    Fix: Fixed cart.html file and one line added to settings.py

--- bio_market_store/bio_market_store/settings.py
+++ bio_market_store/bio_market_store/settings.py
@@ -129,3 +129,4 @@ MEDIA_ROOT = BASE_DIR/"media"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_CURRENCY = 'PLN'
---
+++ bio_market_store/store/tests/test_models.py
@@ -0,0 +1,4 @@
+
+
+from django.test import TestCase
+from store.models import
