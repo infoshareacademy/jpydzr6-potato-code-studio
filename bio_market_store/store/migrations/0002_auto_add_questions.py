b47ae11b5f45fef4bf55c9d6b43bc86cf8277014
Julia <juliablaumann@gmail.com>
2025-02-01 13:07:31 +0100

    Feat: Migrated list of questions to the database

---
+++ bio_market_store/store/migrations/0002_auto_add_questions.py
@@ -0,0 +1,27 @@
+from django.db import migrations
+
+def add_questions(apps, schema_editor):
+    MiniQuizBio = apps.get_model("store", "MiniQuizBio")
+    from store.mini_quiz_bio.list_of_question import list_of_question
+
+    for item in list_of_question:
+        MiniQuizBio.objects.create(
+            question=item.question,
+            answer_choices={
+                "a": item.a,
+                "b": item.b,
+                "c": item.c,
+                "d": item.d
+            },
+            correct_answer=item.correct_answer
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("store", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(add_questions),
+    ]
