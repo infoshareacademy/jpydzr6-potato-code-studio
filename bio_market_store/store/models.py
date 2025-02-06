f4e72ccdcca26e5c08b0a7eebd6eac6f1fb0d05b
Wojciech Wawrzyszko <wuwu1978@o2.pl>
2025-02-06 23:47:30 +0100

    fix: file models.py with new class Product

--- bio_market_store/store/models.py
+++ bio_market_store/store/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 import uuid
-# Create your models here.
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="products")
+    category = models.CharField(max_length=255)
+    name_tag = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    commission = models.DecimalField(decimal_places=2, max_digits=10)
+    weight = models.DecimalField(decimal_places=2, max_digits=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    exp_date = models.DateField(null=True, blank=True)
+    amount = models.IntegerField()
+    producer = models.CharField(max_length=255)
     loyalty_points = models.IntegerField(default=0)
-    picture = models.ImageField(upload_to="img", default="")
+    image = models.ImageField(upload_to="png")
 
     def __str__(self):
-        return self.name
+        return f"{self.name_tag} ({self.category})"
 
 
 class Cart(models.Model):
